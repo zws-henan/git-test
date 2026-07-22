5c5f3457be17d133e036fb258da8d0a1f62e7938
ZuoWenshuang <2357838130@qq.com>
2026-07-23 02:31:12 +0800

    feat:改动了一些细节

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,7 @@ import AppFooter from './components/AppFooter.vue'
 
 // 任务列表数据
 const tasks = ref([
-  { id: 1, text: '学习 JavaScript 基础语法', completed: false },
+  { id: 1, text: '学习 JavaScript 基础语法!!!!!!!!!!!!!', completed: false },
   { id: 2, text: '完成项目设计文档', completed: false },
   { id: 3, text: '回复邮件', completed: false },
   { id: 4, text: '购买日用品', completed: true },
